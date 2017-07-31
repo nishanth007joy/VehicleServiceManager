77068d3f7ddca222bc39bdf5a335dc4807c69797
nishanth007joy <nishanth007joy@gmail.com>
2017-07-31 13:09:12 +0100

    Change-Id: I407d5a40201d58bc1d57a2c2eeebc37da928cae7

---
+++ src/main/java/com/nish/servicemanager/handleradvice/VehicleApiExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.nish.servicemanager.handleradvice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+public class VehicleApiExceptionHandler extends ResponseEntityExceptionHandler{
+
+}
