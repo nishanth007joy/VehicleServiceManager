d78646a5f8ebd23fa5ff8f7d5824719e414b56a1
nishanth007joy <nishanth007joy@gmail.com>
2017-07-31 11:41:34 +0100

    Change-Id: I00224248221d346cae340c4815745e4e71858f98

--- src/main/java/com/nish/servicemanager/app/ServiceManagerApplication.java
+++ src/main/java/com/nish/servicemanager/app/ServiceManagerApplication.java
@@ -1,5 +1,18 @@
 package com.nish.servicemanager.app;
 
-public class ServiceManagerApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class ServiceManagerApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServiceManagerApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceManagerApplication.class, args);
+	}
 }
